c7ea9bdfd9b40de4f7877288f0adf6f643c2ef0b
Fsoler <f.soler@umh.es>
2023-03-10 17:16:10 +0100

    Add MinkDataset

--- Datasets.py
+++ Datasets.py
@@ -155,6 +155,144 @@ class PLYDataset(Dataset):
 
         return features, labels, file.split('.')[0]
 
+
+class minkDataset(Dataset):
+
+    def __init__(self, mode_='train', root_dir = 'my_dataset_dir', features=None, labels=None, normalize=False, binary = False, add_range_=False, voxel_size_=0.05):
+        super().__init__()
+        self.root_dir = root_dir
+        self.add_range = add_range_
+        self.coords = []
+        self.voxel_size = voxel_size_
+        self.mode = mode_
+
+        if features is None:
+            self.features = [0, 1, 2]
+        else:
+            self.features = features
+
+        if labels is None:
+            self.labels = [-1]
+        else:
+            self.labels = labels
+
+        self.normalize = normalize
+        self.binary = binary
+
+        self.dataset = []
+        for file in os.listdir(self.root_dir):
+            if file.endswith(".ply"):
+                self.dataset.append(file)
+
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+    def __getitem__(self, index):
+        file = self.dataset[index]
+        path_to_file = os.path.join(self.root_dir, file)
+        ply = PlyData.read(path_to_file)
+        data = ply["vertex"].data
+        # nm.memmap to np.ndarray
+        data = np.array(list(map(list, data)))
+
+        features = data[:, self.features]
+        self.coords = features[:, [0,1,2]]
+
+        if self.normalize:
+            # XYZ suposed to be 3 first features
+            xyz = features[:, [0,1,2]]
+            centroid = np.mean(xyz, axis=0)
+            xyz -= centroid
+            furthest_distance = np.max(np.sqrt(np.sum(abs(xyz) ** 2, axis=-1)))
+            xyz /= furthest_distance
+            features[:, [0,1,2]] = xyz
+
+        if self.add_range:
+            xyz = features[:, [0,1,2]]
+            D = np.sqrt(np.sum(abs(xyz) ** 2, axis=-1))
+            D = D[:, None]
+            features = np.hstack((features, D))
+
+        if self.mode == 'test_no_labels':
+            return self.coords.astype(np.float32)/self.voxel_size, features.astype(np.float32)
+        
+        else:
+            labels = data[:, self.labels]
+            if self.binary:
+                labels[labels > 0] = 1
+
+            return self.coords.astype(np.float32) / self.voxel_size, features.astype(np.float32), labels.astype(np.int32)
+
+
+class vis_minkDataset(Dataset):
+
+    def __init__(self, root_dir = 'my_dataset_dir',  common_clouds_dir='', extend_clouds=[], features=None, labels=None, normalize=False, binary = False, add_range_=False, voxel_size_=0.05):
+        super().__init__()
+        self.root_dir = root_dir
+        self.add_range = add_range_
+        self.coords = []
+        self.voxel_size = voxel_size_
+
+        if features is None:
+            self.features = [0, 1, 2]
+        else:
+            self.features = features
+
+        if labels is None:
+            self.labels = [-1]
+        else:
+            self.labels = labels
+
+        self.normalize = normalize
+        self.binary = binary
+
+        self.dataset = []
+        for file in os.listdir(common_clouds_dir):
+            if file.endswith(".ply"):
+                self.dataset.append(os.path.join(common_clouds_dir, file))
+        if extend_clouds:
+            for file in extend_clouds:
+                self.dataset.append(os.path.join(self.root_dir, file))
+
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+    def __getitem__(self, index):
+        path_to_file = os.path.abspath(self.dataset[index]) #type: os.path
+        ply = PlyData.read(path_to_file)
+        data = ply["vertex"].data
+        # nm.memmap to np.ndarray
+        data = np.array(list(map(list, data)))
+
+        features = np.copy(data[:, self.features])
+        labels = np.copy(data[:, self.labels])
+        self.coords = np.copy(features[:, [0,1,2]])
+
+        if self.normalize:
+            # XYZ suposed to be 3 first features
+            xyz = features[:, [0,1,2]]
+            centroid = np.mean(xyz, axis=0)
+            xyz -= centroid
+            furthest_distance = np.max(np.sqrt(np.sum(abs(xyz) ** 2, axis=-1)))
+            xyz /= furthest_distance
+            features[:, [0,1,2]] = xyz
+
+        if self.add_range:
+            xyz = features[:, [0,1,2]]
+            D = np.sqrt(np.sum(abs(xyz) ** 2, axis=-1))
+            D = D[:, None]
+            features = np.hstack((features, D))
+
+        if self.binary:
+            labels[labels > 0] = 1
+
+
+        return self.coords.astype(np.float32) / self.voxel_size, features.astype(np.float32), labels.astype(np.int32)
+
 class vis_Test_Dataset(Dataset):
     def __init__(self, root_dir = 'my_dataset_dir', common_clouds_dir='', extend_clouds=[], features=None, labels=None, normalize=False, binary = False, add_range_=False, compute_weights=False):
         super().__init__()
@@ -254,6 +392,8 @@ class vis_Test_Dataset(Dataset):
         # weights = weights/len(labels)
 
         return features, labels, os.path.basename(path_to_file).split('.')[0]
+
+
 class RandDataset(Dataset):
   def __init__(self, n_clouds=50, n_points=3000, n_features=3):
     super(RandDataset, self).__init__()
